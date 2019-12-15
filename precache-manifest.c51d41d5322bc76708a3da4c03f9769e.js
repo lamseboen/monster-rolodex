e873515d211e18d913963ed5fb6670f996755325
lamse <lamse@lamses-MacBook-Pro.local>
2019-12-15 14:26:33 +0700

    Updates

---
+++ precache-manifest.c51d41d5322bc76708a3da4c03f9769e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c57e3c2d47db83d30abd266e3c28f5c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f0386480f36f0a5b4434",
+    "url": "/monster-rolodex/static/css/main.62ce2f47.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0386480f36f0a5b4434",
+    "url": "/monster-rolodex/static/js/main.f65211cf.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
